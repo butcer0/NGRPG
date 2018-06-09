abf2193c762c74c36febb4ce8891a70db31e544f
Erik Butcher <butcer0@gmail.com>
2018-06-09 18:31:59 -0400

    introduced basic attack in fight

--- src/app/components/fight/fight.component.ts
+++ src/app/components/fight/fight.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { GameControllerService } from '../../services/game-controller.service';
+import { Hero, Monster, BaseCharacter, FightOptions, Warrior, Ranger, Rogue, Priest } from '../../models/characters';
+import { Router } from '@angular/router';
+
+enum Teams {
+  heroes,
+  enemies,
+  none
+}
 
 @Component({
   selector: 'fight-component',
@@ -6,10 +15,231 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fight.component.css']
 })
 export class FightComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private gameControllerService: GameControllerService,
+              private router: Router) { }
   ngOnInit() {
+              }
+
+  heroTurn: boolean = true;
+  actionDelay: number = this.gameControllerService.actionDelay;
+  turnsBetweenSpecial = 2;
+  characterIndex: number = 0;
+  freezeActions: boolean = false;
+
+  heroParty: Hero[] = this.gameControllerService.heroParty;
+  heroesIncapacitated: number = 0;
+  enemyParty: Monster[] = this.gameControllerService.enemyParty;
+  enemiesIncapacitated: number = 0;
+
+  currentCharacter: BaseCharacter = this.heroParty[this.characterIndex];
+  _fightOptions: typeof FightOptions = FightOptions;
+  _teams: typeof Teams = Teams;
+  selectedAction: FightOptions = FightOptions.none;
+  availableTargets: Teams = Teams.none;
+  selectedTargets: BaseCharacter[] = [];
+
+  displayMessage: string = `${this.currentCharacter.name}'s turn`;
+  successMessages: string[] = [];
+  showNextChapterButton: boolean = false;
+  showGameOverButton: boolean = false;
+
+  selectOption(selectedOption: FightOptions) {
+    if(this.freezeActions && this.heroTurn) {
+      return;
+    }
+
+    this.selectedAction = selectedOption;
+    this.selectedTargets = [];
+
+    if(this.selectedAction === FightOptions.attack) {
+      this.availableTargets = Teams.enemies;
+      this.displayMessage = "Select a target for your attack";
+    } else if(this.selectedAction === FightOptions.specialAttack
+    && this.currentCharacter instanceof Hero //Erik - 6/9/2018 instanceof is only here to define currentCharacter as Hero so .level is available
+    && this.currentCharacter.level < 3) {
+      this.displayMessage = `Special attacks unlock for a character once they reach level 3`;
+    } else if(this.selectedAction === FightOptions.specialAttack
+    && this.currentCharacter instanceof Hero
+    && this.currentCharacter.level > 2) {
+
+        if(this.currentCharacter.turnsUntilSpecialAvailableAgain) { //Erik - 6/9/2018 If turnsUntil... is 0 -> then this false
+          this.displayMessage = `Cannot use special attack yet. ${this.currentCharacter.turnsUntilSpecialAvailableAgain} left`;
+        } else {
+          if(this.currentCharacter instanceof Warrior) {
+            this.availableTargets = Teams.enemies;
+            this.displayMessage = `Attack two targets at once with a small attack penalty. At level 6 and above, the attack penalty is removed. The two targets may be the same enemy.`;
+          }
+          if(this.currentCharacter instanceof Ranger) {
+            this.availableTargets = Teams.heroes;
+            this.displayMessage = `Setup a trap to protect one of your heroes. The trap will prevent all damage and the enemy will take a turn to free itself from the trap. At level 6 and above, the trap will also deal up to 8 damage.`;
+          }
+          if(this.currentCharacter instanceof Rogue) {
+            this.availableTargets = Teams.enemies;
+            this.displayMessage = `Poison an enemy or add another stack of poison to an enemy to do up to 3 damage, with each stack of poison multiplying damage. At level 6 and above, the damage is up to 1-6 times the number of poison stacks.`;
+          }
+          if(this.currentCharacter instanceof Priest) {
+            this.availableTargets = Teams.heroes;
+            this.displayMessage = `Select a hero to heal for up to 6 health plus an additional point for each point in the intelligence skill. At level 6 and above, you choose two targets to heal. The two targets can be the same hero.`;
+          }
+        }
+    }
+
+  }
+ 
+  tryAttack(target: BaseCharacter) {
+    if(this.freezeActions) {
+      return;
+    }
+    if(target.isIncapacitated) {
+      this.displayMessage = "That target is already incapacitated.";
+      return;
+    }
+
+    if(this.currentCharacter instanceof Monster && target instanceof Hero) {
+
+    }
+
+    if(this.selectedAction === FightOptions.attack) {
+      this.freezeActions = true;
+      this.attack(target);
+    } else if(this.currentCharacter instanceof Hero) {
+    } else {
+      this.displayMessage = `Please Select an action option`;
+    }
   }
 
+  attack(target: BaseCharacter) {
+    this.availableTargets = Teams.none;
+    if(this.currentCharacter.attack() >= target.barriers.attack) {
+      let damage = this.currentCharacter.dealDamage();
+      target.currentHealth -= damage;
+      this.displayMessage = `${this.currentCharacter.name} hit ${target.name} dealing ${damage} damage.`;
+      setTimeout(() => {
+        if(target.currentHealth <= 0) {
+          target.isIncapacitated = true;
+          this.heroTurn ? this.enemiesIncapacitated++ : this.heroesIncapacitated++;
+          this.checkIfWin();
+        } else {
+          this.showNextChapterButton();
+        }
+      }, this.actionDelay);
+    } else {
+      this.displayMessage = `${this.currentCharacter.name} Missed.`;
+      setTimeout(() => {
+        this.nextTurn();
+      }, this.actionDelay);
+    }
+  }
+
+  checkIfWin() {
+    this.selectedAction = FightOptions.none;
+    if(this.enemiesIncapacitated === this.enemyParty.length) {
+      this.displayMessage = `All enemies have been defeated!`;
+      this.successMessages = this.gameControllerService.encounterSuccess();
+      this.showNextChapterButton = true;
+      this.gameControllerService.isFighting = false;
+      return;
+    }
+    if(this.heroesIncapacitated === this.heroParty.length) {
+      this.displayMessage = `All heroes have been defeated!`;
+      this.showGameOverButton = true;
+      this.gameControllerService.isFighting = false;
+      return;
+    }
+    this.nextTurn();
+  }
+
+  nextTurn() {
+    if(this.currentCharacter instanceof Monster
+      && this.currentCharacter.poisonStacks
+    && !this.currentCharacter.hasTakenPoisonDamageThisTurn) {
+      this.currentCharacter.hasTakenPoisonDamageThisTurn = true;
+      let maxDamage = this.currentCharacter.isStrongPoisoned ? 6 : 3;
+      let poisonDamage = (Math.floor(Math.random() * maxDamage) + 1) * this.currentCharacter.;
+      this.currentCharacter.currentHealth -= poisonDamage;
+      this.displayMessage = `${this.currentCharacter.name} tool ${poisonDamage} poison damage!`;
+      if(this.currentCharacter.currentHealth <= 0) {
+        this.currentCharacter.isIncapacitated = true;
+        this.enemiesIncapacitated++;
+      }
+      setTimeout((function() {
+        this.checkIfWin();
+      }).bind(this), this.actionDelay);
+      return;
+    }
+    if(this.currentCharacter instanceof Monster &&
+    this.currentCharacter.hasTakenPoisonDamageThisTurn) {
+
+    }
+
+    this.availableTargets = Teams.none;
+    this.selectedAction = FightOptions.none;
+    this.characterIndex++;
+    let nextCharacter;
+
+    if(this.heroTurn) {
+      nextCharacter = this.heroParty[this.characterIndex];
+    } else {
+      nextCharacter = this.heroParty[this.characterIndex];
+    }
+
+    if(nextCharacter){
+      if(!nextCharacter.isIncapacitated) {
+        this.currentCharacter = nextCharacter;
+        this.displayMessage = `It's ${this.currentCharacter.name}'s turn.`;
+        if(this.currentCharacter instanceof Hero) {
+          this.freezeActions = false;
+          if(this.currentCharacter.turnsUntilSpecialAvailableAgain) {
+            this.currentCharacter.turnsUntilSpecialAvailableAgain--;
+          }
+        } else {
+          setTimeout(() => {
+            this.takeEnemyTurn();
+          }, this.actionDelay);
+        }
+      } else {
+        this.nextTurn();
+      }
+    } else {
+      this.heroTurn = !this.heroTurn;
+      this.characterIndex = -1;
+      this.nextTurn();
+    }
+  }
+
+  takeEnemyTurn() {
+    if(this.currentCharacter instanceof Monster && this.currentCharacter.isTrapped) {
+      this.currentCharacter.isTrapped = false;
+      this.displayMessage = `${this.currentCharacter.name} freed itself from the trap.`;
+      setTimeout(() => {
+        this.nextTurn();
+      }, this.actionDelay);
+    } else {
+      let target: Hero;
+      this.selectedAction = FightOptions.attack;
+
+      while(!target) {
+        let randomTargetIndex = Math.floor(Math.random() * this.heroParty.length);
+        let potentialTarget = this.heroParty[randomTargetIndex];
+        if(!potentialTarget.isIncapacitated) {
+          target = potentialTarget;
+        }
+      }
+      this.displayMessage = `${this.currentCharacter.name} attacks ${target.name}.`;
+      
+      setTimeout(() => {
+        this.tryAttack(target);
+      }, this.actionDelay);
+
+    }
+  }
+
+  nextChapter() {
+    this.gameControllerService.nextChapter();
+    this.router.navigateByUrl("/story");
+  }
+
+  gameOver() {
+    this.gameControllerService.gameOver();
+  }
 }
