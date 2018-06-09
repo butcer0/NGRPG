3502791886dc1aa178308ca05dadebfeba7b0034
Erik Butcher <butcer0@gmail.com>
2018-06-09 12:53:10 -0400

    updated character creation

--- src/app/components/character-creation/character-creation.component.ts
+++ src/app/components/character-creation/character-creation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterOptions } from '../../models/character-options';
+import { isUndefined } from 'util';
+
+const DEFAULTSELECTIONTEXT: string = "--Choose--";
 
 @Component({
   selector: 'character-creation-component',
@@ -6,10 +10,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-creation.component.css']
 })
 export class CharacterCreationComponent implements OnInit {
+  character = {
+    race: DEFAULTSELECTIONTEXT,
+    class: DEFAULTSELECTIONTEXT,
+    gender: undefined,
+    name: undefined
+  }
+
+  characterComplete: boolean = false;
+
+  races = CharacterOptions.races;
+  classes = CharacterOptions.classes;
+  genders = CharacterOptions.genders;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  changeRace(race: string):void {
+    this.character.race = race;
+    this.checkCompleted();
+  }
+
+  changeGender(gender: string):void {
+    this.character.gender = gender;
+    this.checkCompleted();
+  }
+
+  changeClass(newClass: string):void {
+    this.character.class = newClass;
+    this.checkCompleted();
+  }
+  
+  checkCompleted(): void {
+    this.characterComplete = this.character.race !== DEFAULTSELECTIONTEXT 
+                        && this.character.class !== DEFAULTSELECTIONTEXT
+                        && this.character.gender
+                        && this.character.name;
+
+  }
+
+  changeName():void {
+    this.checkCompleted();
+  }
+
+  createCharacter():void {
+    if(!this.characterComplete)
+    {
+      return;
+    }
+
+    console.log(this.character);
+  }
+
+  
+
+  
 
 }
